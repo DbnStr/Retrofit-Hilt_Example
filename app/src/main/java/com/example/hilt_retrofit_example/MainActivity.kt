84766a810798001045032772296a38e7cc665421
DBNSTR <alexpich2002@yandex.ru>
2021-07-18 22:30:51 +0300

    navigation and entryPoints

--- app/src/main/java/com/example/hilt_retrofit_example/MainActivity.kt
+++ app/src/main/java/com/example/hilt_retrofit_example/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.hilt_retrofit_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.EntryPoint
 
+@EntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
