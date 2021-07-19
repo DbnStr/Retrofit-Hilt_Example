2c5bd3b86e388e1b16db663203b445c521ce6e64
DBNSTR <alexpich2002@yandex.ru>
2021-07-19 14:45:30 +0300

    finish

--- app/src/main/java/com/example/hilt_retrofit_example/MainFragment.kt
+++ app/src/main/java/com/example/hilt_retrofit_example/MainFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import org.w3c.dom.Text
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -17,10 +20,14 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val constraintLayout = inflater.inflate(R.layout.fragment_main, container, false)
+                as ConstraintLayout
+        val name : TextView = constraintLayout.findViewById(R.id.name_field)
+        val age : TextView = constraintLayout.findViewById(R.id.age_field)
         viewModel.getUser(0).observe(viewLifecycleOwner, {user ->
-            println(user.age)
+            name.text = user.name
+            age.text = user.age.toString()
         })
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return constraintLayout
     }
 }
